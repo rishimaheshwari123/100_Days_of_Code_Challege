72db73de0b4b82889e03c392e46c3b72e7c4b1be
Rishi <114659322+rishimaheshwari123@users.noreply.github.com>
2022-10-11 16:11:59 +0530

    Create UpperCase.java

---
+++ Day1_String_Methods_UpperCase_LowerCase/UpperCase.java
@@ -0,0 +1,30 @@
+Given a string s, return the string after replacing every lowercase letter with the same uppercase letter.
+
+ 
+
+Example 1:
+
+Input: s = "hello"
+Output: "Hello"
+Example 2:
+
+Input: s = "RISHI"
+Output: "RISHI"
+Example 3:
+
+Input: s = "lovely"
+Output: "LOVELY"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of printable ASCII characters.
+  
+  code:-
+    
+  class Solution {
+    public String toUpperCase(String s) {
+     return  s.toUpperCase();  
+    }
+}
