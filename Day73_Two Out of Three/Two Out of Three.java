92bb6fcede882525dbba5835dafb59bbc5f8e52e
Rishi <114659322+rishimaheshwari123@users.noreply.github.com>
2022-12-24 20:43:53 +0530

    Create Two Out of Three.java

---
+++ Day73_Two Out of Three/Two Out of Three.java
@@ -0,0 +1,64 @@
+/*
+Question -> 
+Given three integer arrays nums1, nums2, and nums3, return a distinct array containing all the values that are present in at least two out of the three arrays. You may return the values in any order.
+ 
+
+Example 1:
+
+Input: nums1 = [1,1,3,2], nums2 = [2,3], nums3 = [3]
+Output: [3,2]
+Explanation: The values that are present in at least two arrays are:
+- 3, in all three arrays.
+- 2, in nums1 and nums2.
+Example 2:
+
+Input: nums1 = [3,1], nums2 = [2,3], nums3 = [1,2]
+Output: [2,3,1]
+Explanation: The values that are present in at least two arrays are:
+- 2, in nums2 and nums3.
+- 3, in nums1 and nums2.
+- 1, in nums1 and nums3.
+Example 3:
+
+Input: nums1 = [1,2,2], nums2 = [4,3,3], nums3 = [5]
+Output: []
+Explanation: No value is present in at least two arrays.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length, nums3.length <= 100
+1 <= nums1[i], nums2[j], nums3[k] <= 100
+ */ 
+  
+  Solution -> 
+    
+  class Solution {
+      	public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        
+        
+        List<Integer> list = new ArrayList();
+        
+        Set<Integer> set1 = new HashSet();
+        Set<Integer> set2 = new HashSet();
+        for(int num : nums1){
+            set1.add(num);
+        }
+        
+        for(int num: nums2){
+            set2.add(num);
+            if(set1.contains(num) && !list.contains(num)){
+                list.add(num);
+            }
+        }
+        
+        for(int num : nums3){
+            if((set2.contains(num) && !list.contains(num)) || (set1.contains(num) && !list.contains(num))){
+                list.add(num);
+            }
+        }
+        
+        return list;
+    }
+	
+}
